49bdc935d61411ce50cd546e056ba5650f3880a8
Ciro Costa <ciro.costa@usp.br>
2014-01-18 19:29:42 -0200

    fixing Files

--- extension/js/mark_as_read_cs.js
+++
@@ -1,6 +0,0 @@
-/** Contains all of the code for the content_script to be used in those
-	pages that were added to the database to be read latter. 
-*/
-
-console.log("mark as read CS!");
-alert('lol');
